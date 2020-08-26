12d2968c755f3495ce0c678177b1d294dfd85f48
HHenry072 <33395929+HHenry072@users.noreply.github.com>
2020-08-26 17:38:00 +0200

    Added ability to choose the detection module

--- WebApp/pages/views.py
+++ WebApp/pages/views.py
@@ -5,6 +5,7 @@ from django.core.files.storage import FileSystemStorage
 from .tf_hub import run_object_detection
 import os
 from .semanticCaller import semanticCaller
+import tempfile
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -14,33 +15,45 @@ def convertList_toHTML(mylist):
     return a
 
 #Create your views here
+
 def upload(request):
     context = {}
     if request.method == 'POST':
-        # Save the File
+
+        # Request Detection Type from the Radio Buttons/User Input
+        modul = request.POST["modul"]
+
+        # Get the file and upload it to a temporary file
         uploaded_file = request.FILES['inpFile']
-        fs = FileSystemStorage()
-        name = fs.save(uploaded_file.name, uploaded_file)
+        #fs = FileSystemStorage()
+        #name = fs.save(uploaded_file.name, uploaded_file)
+
+
         # Define the path for the loaded image and for the result image
         path_to_image = os.path.join(BASE_DIR, 'media/{0}'.format(uploaded_file.name))
         path_to_save = os.path.join(BASE_DIR, 'media/results/')
+
         # Run Object Detection
-        # 1 for accurate Detection, 2 for fast Detection
-        ## (How to import the radio button here so you can choose between fast and accurate?)
-        ObjList = run_object_detection(2, path_to_image, path_to_save)
+        ObjList = run_object_detection(int(modul), uploaded_file, path_to_save)
+        #ObjList = ObjDet[0]
+        #Image_with_Boxes = ObjDet[1]
+
         # Convert the List to display in the Output Field
         ObjListHTML = convertList_toHTML(ObjList)
 
         # Get Scenes from the SemanticAPI
         SemaList = semanticCaller(ObjList)
+        # Convert the List to display in the Ouput Field
         for List in SemaList:
             SemaListHTML = convertList_toHTML(List)
 
+        # Function to delete the saved files
 
         context= {
             'url' : fs.url(name),
             'ObjListHTML' : ObjListHTML,
             'SemaListHTML': SemaListHTML
+           # 'Image_with_Boxes': Image_with_Boxes
          }
 
     return render(request, 'upload.html', context)
