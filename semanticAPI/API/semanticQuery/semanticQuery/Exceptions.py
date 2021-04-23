0e1704c8543dde651f416723ad91e4d93127a8b8
Achim Reiz <areiz@informatik.uni-rostock.de>
2021-04-23 12:24:29 +0200

    Semantic TestCase + removed unnecessary Code

--- semanticAPI/API/semanticQuery/semanticQuery/Exceptions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.exceptions import APIException
-
-class WrongInputData(APIException):
-    status_code = 400
-    default_detail = 'Wrong Input data provided. the objectID1 as an identifier'
